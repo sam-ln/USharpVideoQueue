2938508814f574eea6d0d0e43ecdc34df9633269
sam-ln <82455742+sam-ln@users.noreply.github.com>
2022-11-26 16:27:08 +0100

    Added basic infrastructure for tdd, Queue stub

---
+++ Assets/USharpVideoQueue/Runtime/VideoQueue.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UdonSharp.Video;
+
+namespace USharpVideoQueue.Runtime
+{
+    public class VideoQueue : UdonSharpBehaviour
+    {
+  
+        internal bool Initialized;
+        internal void Start()
+        {
+            Initialized = true;
+            Debug.Log("Started");
+        }
+
+        internal void DummyMethod() {
+            
+        }
+    }
+}
---
+++ Assets/USharpVideoQueue/Tests/Editor/BasicTest.cs
@@ -0,0 +1,45 @@
+﻿
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using USharpVideoQueue.Runtime;
+using VRC.SDKBase;
+using VRC.Udon;
+using UdonSharp;
+
+namespace USharpVideoQueue.Tests.Editor
+{
+    public class BasicTest
+    {
+        private VideoQueue queue;
+
+        [SetUp]
+        public void Prepare() {
+            queue = new GameObject().AddComponent<VideoQueue>();
+            queue.Start();
+        }
+
+        /**
+        * 
+        */
+        [Test]
+        public void CreateBehavior()
+        {  
+            Assert.NotNull(queue);
+            Assert.True(queue.Initialized);    
+        }
+
+
+        /*
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator BasicTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+        */
+    }
+}
